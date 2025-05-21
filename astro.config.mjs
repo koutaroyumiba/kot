f4bf775768964e60efbb3479a5e82339dd74f395
Kot <89110272+Kot6603@users.noreply.github.com>
2025-05-22 10:17:08 +1200

    chore(deployment): fixed paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kot6603.github.io",
-  base: "kot",
+  site: "https://koutaroyumiba.com",
 });
