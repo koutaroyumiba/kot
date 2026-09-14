5aa4f33e2616dee7f54de5827e5bef97b86b857e
Kot <89110272+koutaroyumiba@users.noreply.github.com>
2026-09-14 16:17:58 +1200

    feat(v3): add thoughts ideas backlog

---
+++ src/data/thoughts.ts
@@ -0,0 +1,137 @@
+export interface Thoughts {
+  id: number;
+  title: string;
+}
+
+export const thoughts: Thoughts[] = [
+  {
+    id: 1,
+    title: "Paradox of Tolerance",
+  },
+  {
+    id: 2,
+    title: "On the Importance of Reading",
+  },
+  {
+    id: 3,
+    title: "Thoughts on Nietzsche's theory on the Will to Power",
+  },
+  {
+    id: 4,
+    title: "The Importance in the Attainment of New Knowledge",
+  },
+  {
+    id: 5,
+    title: "On the Importance of Education",
+  },
+  {
+    id: 6,
+    title: 'On the Difficulty of Pursuing "Truth"',
+  },
+  {
+    id: 7,
+    title: "On the Pleasures and Joys of Mankind",
+  },
+  {
+    id: 8,
+    title: "The Grounds of Justice as Questioned by Plato",
+  },
+  {
+    id: 9,
+    title: "On the Fundamental Structure of Economic Infrastructure",
+  },
+  {
+    id: 10,
+    title: "On the Importance of Writing",
+  },
+  {
+    id: 11,
+    title: "On the Importance of Ambiguity in Language",
+  },
+  {
+    id: 12,
+    title: "On the Modern View of Entertainment",
+  },
+  {
+    id: 13,
+    title: "On the Dichotomy between Thinking and Experience",
+  },
+  {
+    id: 14,
+    title: 'On the Ideal Way of Combatting "Evil"',
+  },
+  {
+    id: 15,
+    title: "View of Suffering as Empowerment",
+  },
+  {
+    id: 16,
+    title:
+      "Trust as a Resource and the Foundation of a Successful Relationship",
+  },
+  {
+    id: 17,
+    title:
+      "On Self-Awareness and lack thereof, and our Powerlessness to View Life Objectively",
+  },
+  {
+    id: 18,
+    title: "On Teachers and Mentors, and the Roles of Students",
+  },
+  {
+    id: 19,
+    title: "On the Freedom of Individuals or the Perceived Freedom",
+  },
+  {
+    id: 20,
+    title: "On the Dichotomy between Instinct and Reason",
+  },
+  {
+    id: 21,
+    title: "On Virtue and the Cardinal Good",
+  },
+  {
+    id: 22,
+    title: "On Vices and the Cardinal Bad",
+  },
+  {
+    id: 23,
+    title: "On the Concept of Free Will and its Consequences",
+  },
+  {
+    id: 24,
+    title: "On the Utmost Importance of Knowledge and Honesty in Democracy",
+  },
+  {
+    id: 25,
+    title: "On Self-Esteem and its Rivalry to Self-Respect",
+  },
+  {
+    id: 26,
+    title: 'A Selfish Comment on the Ignorant and the "Special"',
+  },
+  {
+    id: 27,
+    title: "A Political Perspective on the Realm of Ideas",
+  },
+  {
+    id: 28,
+    title: "On the Similarities and Lessons for Life from Water",
+  },
+  {
+    id: 29,
+    title: "On Sympathy and the Balance between Competition and Collaboration",
+  },
+  {
+    id: 30,
+    title: "Time Takes Away the Pain",
+  },
+  {
+    id: 31,
+    title: "Racism as a Determinant of Culture",
+  },
+  {
+    id: 32,
+    title: "Views of Relationships through the Concept of Home",
+  },
+];
