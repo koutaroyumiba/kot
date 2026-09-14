35121b598d8f066f4a7d8f4437c7ce784a15fb4a
Kot <89110272+koutaroyumiba@users.noreply.github.com>
2026-09-14 20:51:05 +1200

    refactor(v3): remove unused React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://koutaroyumiba.com",
-  integrations: [react()],
 
   markdown: {
     shikiConfig: {
